e002181de2ef5b65f42a091a91f8f8a1ad3d5952
Natalya Storchilova <nata@Storcilovas-MacBook-Pro.local>
2020-09-19 10:05:46 +0300

    Added date check

--- src/test/java/ru/netology/CallBackTest.java
+++ src/test/java/ru/netology/CallBackTest.java
@@ -1,9 +1,12 @@
 package ru.netology;
 
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
-import static com.codeborne.selenide.Condition.exactText;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -14,15 +17,21 @@ public class CallBackTest {
     @Test
     void shouldSubmitRequest() {
         open("http://localhost:9999");
-        SelenideElement form = $("[id=root]");
         $("[data-test-id='city'] input").setValue("Москва");
-        $("[data-test-id='date']");
+//        Очищаем поле ввода даты
+        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+//        Генерируем строку с датой
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+//        Ввод сгенерированной даты
+        $("[data-test-id=date] input.input__control").setValue(date);
         $("[data-test-id='name'] input").setValue("Анна Иванова");
         $("[data-test-id='phone'] input").setValue("+79001234567");
         $("[data-test-id=agreement]").click();
         $(byText("Забронировать")).click();
 
         $(withText("Успешно!")).waitUntil(visible, 15000);
+//        Проверка даты в сплывающем сообщении
+        $("[data-test-id=notification] .notification__content").waitUntil(visible, 15000).shouldHave(text(date));
     }
 
 }
